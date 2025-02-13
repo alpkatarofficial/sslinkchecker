11508af642f712539478c4335892c43d6597ff10
alpkatarofficial <alpharveyspecter@gmail.com>
2025-02-13 10:54:26 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -13,17 +13,4 @@ app = create_app()
 if __name__ == '__main__':
     print(f'Running server on port {port}...')
     app.serve_forever()
-
-# Rest of your original app.py code stays exactly the same
-def get_ssl_dates(hostname):
-    # ... (your existing code)
-
-class CustomHandler(SimpleHTTPRequestHandler):
-    # ... (your existing code)
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
-    server_address = ('', port)
-    httpd = HTTPServer(server_address, CustomHandler)
-    print(f'Running server on port {port}...')
-    httpd.serve_forever()
+    
