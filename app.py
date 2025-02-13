6f60e370a0b58094559ca654c5c2ba48e051e336
alpkatarofficial <alpharveyspecter@gmail.com>
2025-02-13 11:03:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from datetime import datetime, timezone, timedelta
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 import urllib.parse as urlparse
 import json
+from wsgiref.simple_server import make_server
 
 def get_ssl_dates(hostname):
     context = ssl.create_default_context()
@@ -46,10 +47,18 @@ class CustomHandler(SimpleHTTPRequestHandler):
         else:
             super().do_GET()
 
+def make_wsgi_app():
+    def simple_app(environ, start_response):
+        status = '200 OK'
+        headers = [('Content-type', 'text/plain; charset=utf-8')]
+        start_response(status, headers)
+        return [b"Hello, World!"]
+
+    return simple_app
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     server_address = ('', port)
     httpd = HTTPServer(server_address, CustomHandler)
     print(f'Running server on port {port}...')
     httpd.serve_forever()
-
