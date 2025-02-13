c8baa3af25d22649b75dcd3c826b749865696664
alpkatarofficial <alpharveyspecter@gmail.com>
2025-02-13 11:20:56 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 # wsgi.py
-from app import CustomHandler, HTTPServer, make_wsgi_app
+from app import make_wsgi_app
 import os
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 def create_app():
     return make_wsgi_app()
@@ -10,7 +11,7 @@ app = create_app()
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     server_address = ('', port)
-    httpd = HTTPServer(server_address, CustomHandler)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
     print(f'Running server on port {port}...')
     httpd.serve_forever()
 
